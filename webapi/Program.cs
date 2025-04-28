16d2678c6a75099fd8917534c13b72884397be71
Jonathan Guzman <jonguz5@hotmail.com>
2025-04-28 16:35:21 -0600

    enable cors

--- webapi/Program.cs
+++ webapi/Program.cs
@@ -2,7 +2,20 @@ using System.Text.Json;
 using System.Text.Json.Serialization;
 using webapi.db.connection;
 
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+	options.AddPolicy(name: MyAllowSpecificOrigins,
+					  policy =>
+					  {
+						  policy.WithOrigins("http://localhost:3000")
+												.AllowAnyHeader()
+												.AllowAnyMethod();
+					  });
+});
 
 builder.Services.AddScoped(provider =>
 {
@@ -41,6 +54,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors(MyAllowSpecificOrigins);
 
 app.MapControllers();
 
